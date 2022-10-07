ac27cd4fad3962adc8a135ca77f2be8519677472
yuto32 <110207144+yuto32@users.noreply.github.com>
2022-10-07 09:43:46 +0900

    Add files via upload

---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+	write(1, "\n", 1);
+}
+
+
